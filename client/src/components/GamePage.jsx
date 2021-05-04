534519393fe329f70de5b0ceb29bf0574afd78c7
Shaan Thakker <shaan38t@gmail.com>
2021-05-04 08:42:33 -0400

    fixed url

--- client/src/components/GamePage.jsx
+++ client/src/components/GamePage.jsx
@@ -5,7 +5,7 @@ import Lobby from './Lobby'
 import Loader from "react-loader-spinner";
 import io from 'socket.io-client'
 
-const socket = io('https://triviahit.games/')
+const socket = io('/')
 const axios = require('axios')
 
 export default function GamePage({match}) {
